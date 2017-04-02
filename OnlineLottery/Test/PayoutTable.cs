6e4c6fc799db5719a2ff51c31ca93468e2603a43
Dany Abplanalp <d.abplanalp@hotmail.com>
2017-04-02 23:20:01 +0200

    Add FitNesse testsuite to version control.

---
+++ OnlineLottery/Test/PayoutTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OnlineLottery.Test
+{
+    public class PayoutTable : fit.ColumnFixture
+    {
+        private readonly WinningsCalculator _wc = new WinningsCalculator();
+
+        public int WinningCombination;
+        public decimal? PayoutPool;
+
+        public int PoolPercentage()
+        {
+            return _wc.GetPoolPercentage(WinningCombination);
+        }
+
+        public decimal PrizePool()
+        {
+            if (PayoutPool == null) PayoutPool = decimal.Parse(Args[0]);
+            return _wc.GetPrizePool(WinningCombination, PayoutPool.Value);
+        }
+    }
+}
