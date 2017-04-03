d763ffd8267d95ccf977d93812321952d8dff428
Dany Abplanalp <d.abplanalp@hotmail.com>
2017-04-03 23:31:57 +0200

    Player Registration and LogIn

---
+++ OnlineLottery/PlayerRegistrationInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineLottery
+{
+    public class PlayerRegistrationInfo : IPlayerRegistrationInfo
+    {
+        public string Name { get; }
+        public string Address { get; }
+        public string City { get; }
+        public string PostCode { get; }
+        public string Country { get; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ OnlineLottery/Test/PlayerRegistration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using fit;
+
+namespace OnlineLottery.Test
+{
+    public class SetUpTestEnvironment : Fixture
+    {
+        internal static IPlayerManager PlayerManager;
+        public SetUpTestEnvironment()
+        {
+            PlayerManager = new PlayerManager();
+        }
+    }
+
+    public class PlayerRegisters : ColumnFixture
+    {
+        public string Username = "";
+        public string Password = "";
+
+        public int PlayerId()
+        {
+            return SetUpTestEnvironment.PlayerManager.RegisterPlayer(new PlayerRegistrationInfo
+            {
+                Username = Username,
+                Password = Password
+            });
+        }
+    }
+
+    public class CheckStoredDetails : ColumnFixture
+    {
+        public int PlayerId;
+        public string Username => SetUpTestEnvironment.PlayerManager.GetPlayer(PlayerId).Username;
+
+        public decimal Balance => SetUpTestEnvironment.PlayerManager.GetPlayer(PlayerId).Balance;
+    }
+
+    public class CheckLogIn : ColumnFixture
+    {
+        public string Username = "";
+        public string Password = "";
+
+        public bool CanLogIn()
+        {
+            try
+            {
+                SetUpTestEnvironment.PlayerManager.LogIn(Username, Password);
+                return true;
+            }
+            catch (ApplicationException)
+            {
+                return false;
+            }
+        }
+    }
+}
