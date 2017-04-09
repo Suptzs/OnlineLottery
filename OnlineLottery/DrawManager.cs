f4698207a73053f78748de060fe36e3da8c1c0c3
Dany Abplanalp <d.abplanalp@hotmail.com>
2017-04-10 00:57:10 +0200

    DrawManager can settle draw and pay prizemoney to the players with winning tickets

--- OnlineLottery/DrawManager.cs
+++ OnlineLottery/DrawManager.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OnlineLottery
 {
     public class DrawManager : IDrawManager
     {
+        public decimal OperatorDeductionFactor => 0.5m;
+
         private readonly IPlayerManager _playerManager;
         private readonly Dictionary<DateTime, Draw> _draws = new Dictionary<DateTime, Draw>();
 
@@ -28,13 +31,57 @@ namespace OnlineLottery
         public void PurchaseTicket(DateTime drawDate, int playerId, int[] numbers, decimal value)
         {
             if (!_draws.ContainsKey(drawDate)) throw new DrawNotOpenException();
-            if(numbers.Length != 6) throw new WrongAmountOfNumbersException();
-            if(value < 0) throw new InvalidPurchaseException();
+            if (numbers.Length != 6) throw new WrongAmountOfNumbersException();
+            if (value < 0) throw new InvalidPurchaseException();
 
             var d = _draws[drawDate];
             var player = _playerManager.GetPlayer(playerId);
             _playerManager.AdjustBalance(playerId, -value);
             d.AddTicket(new Ticket(player, drawDate, numbers, value));
         }
+
+        public void SettleDraw(DateTime drawDate, int[] results)
+        {
+            var d = _draws[drawDate];
+            d.IsOpen = false;
+            var ticketCategories = SplitTicketsIntoCategories(results, d.Tickets);
+            for (var commonNumbers = 0; commonNumbers <= results.Length; commonNumbers++)
+                SettleTicketCategories(commonNumbers, d, ticketCategories[commonNumbers]);
+        }
+
+        private void SettleTicketCategories(int commonNumbers, IDraw draw, List<ITicket> tickets)
+        {
+            var prizePool = new WinningsCalculator().GetPrizePool(commonNumbers, draw.TotalPoolSize * (1 - OperatorDeductionFactor));
+            var totalTicketValue = GetTotalTicketValue(tickets);
+            foreach (Ticket t in tickets)
+                SettleTicket(t, prizePool, totalTicketValue);
+        }
+
+        private void SettleTicket(Ticket t, decimal prizePool, decimal totalTicketValue)
+        {
+            t.IsOpen = false;
+            if (prizePool <= 0) return;
+
+            t.Winnings = t.Value * prizePool / totalTicketValue;
+            _playerManager.AdjustBalance(t.Holder.PlayerId, t.Winnings);
+        }
+
+        private static Dictionary<int, List<ITicket>> SplitTicketsIntoCategories(int[] results, IEnumerable<ITicket> tickets)
+        {
+            var ticketCategories = new Dictionary<int, List<ITicket>>();
+
+            for (var i = 0; i <= results.Length; i++)
+                ticketCategories[i] = new List<ITicket>();
+
+            foreach(var t in tickets)
+                ticketCategories[CountCommonElements(t.Numbers, results)].Add(t);
+
+            return ticketCategories;
+        }
+
+        private static int CountCommonElements(IEnumerable<int> ticketNumbers, int[] results)
+            => ticketNumbers.Sum(ticketNumber => results.Count(result => result == ticketNumber));
+
+        private static decimal GetTotalTicketValue(IEnumerable<ITicket> tickets) => tickets.Sum(t => t.Value);
     }
 }
